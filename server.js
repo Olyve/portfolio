f6312600e2b3f8bfde482d482e413bf50b772128
Sam Galizia <samgalizia@gmail.com>
2017-11-09 16:21:03 -0800

    Set up static serving of html

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send("Hello, world!"));
+// Set up static directory to serve pages
+app.use(express.static('public'));
+
+// For the root route, return the index page
+app.get('/', function(req, res) {
+		res.sendFile(path.join('/public', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
